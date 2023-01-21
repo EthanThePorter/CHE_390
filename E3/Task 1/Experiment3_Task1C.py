ec5b01005f488b24b7424922b1e642c80dd7a2f6
ethanporter <ethantheporter@outlook.com>
2023-01-21 18:59:51 -0500

    V5

--- E3/Task 1/Experiment3_Task1C.py
+++ E3/Task 1/Experiment3_Task1C.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit, fsolve
-
 import matplotlib.pyplot as plt
 import openpyxl
 
@@ -43,13 +42,12 @@ def getWilsonParameters():
     G0 = [0.1, 0.1]
     # Fit data to get parameters for Wilson equation
     G, covariance = curve_fit(Wilson, x, qexpt, G0)
-
     return G
 
 
 def WilsonEquations(x, G12, G21):
-    gamma1 = np.log(-np.log(x + (1 - x) * G12) + (1 - x) * (G12 / (x + (1 - x) * G12) - G21 / ((1 - x) + x * G21)))
-    gamma2 = np.log(-np.log((1 - x) + x * G21) - x * (G12 / (x + (1 - x) * G12) - G21 / ((1 - x) + x * G21)))
+    gamma1 = np.exp(-np.log(x + (1 - x) * G12) + (1 - x) * (G12 / (x + (1 - x) * G12) - G21 / ((1 - x) + x * G21)))
+    gamma2 = np.exp(-np.log((1 - x) + x * G21) - x * (G12 / (x + (1 - x) * G12) - G21 / ((1 - x) + x * G21)))
     return gamma1, gamma2
 
 
@@ -68,34 +66,47 @@ def get_VLE_data(parameters, P_total, G, x):
     P1 = 10 ** (7.28781 - 1623.22 / (T - 44.170))
     # Vapor pressure of pure water
     P2 = 10 ** (7.19621 - (1730.63 / (T - 39.724)))
-    print(gamma1, gamma2)
     return P_total - gamma1 * x1 * P1 / y1, P_total - gamma2 * x2 * P2 / y2
-
 
 
 # Define total system pressure (kPa)
 P_total = 32.86
 # Define points to plot T-xy and xy data
-x_array = np.arange(0, 1, 0.05)
+x_array = np.arange(0.001, 0.999, 0.001)
 # Get parameters for Wilson equation
 G = getWilsonParameters()
 # Initialize list to save data to
 T_list = []
 y_list = []
-# Initialize guess for y and T
-initial_guess = [325, 0.1]
-# # Get VLE data
-# for x in x_array:
-#     T, y = fsolve(get_VLE_data, x0=initial_guess, args=(P_total, G, x))
-#     T_list.append(T)
-#     y_list.append(y)
-# # Plot
-# plt.plot(x_array, T)
-# plt.plot(y_list, T)
-# plt.show()
+# Get VLE data
+for x in x_array:
+    T, y = fsolve(get_VLE_data, x0=(344, x), args=(P_total, G, x))
+    T_list.append(T)
+    y_list.append(y)
 
-x = 0.063
-print(x)
-parameters = fsolve(get_VLE_data, x0=initial_guess, args=(P_total, G, x))
-print(get_VLE_data(parameters, P_total, G, x))
-print(parameters)
+# Get VLE data from literature
+data = pd.read_excel('./data/VLEData_EthanolWater2.xlsx', 'Sheet1')
+# Parse data
+Td = data.iloc[1:21, 0]
+xd = data.iloc[1:21, 1]
+yd = data.iloc[1:21, 2]
+# Convert data to arrays
+T_actual = np.asarray(Td, dtype=float)
+x_actual = np.asarray(xd, dtype=float)
+y_actual = np.asarray(yd, dtype=float)
+
+# Output R-squared for plots
+
+
+# Plot
+plt.plot(x_array, T_list, label='Predicted Bubble Point Curve')
+plt.plot(y_list, T_list, label='Predicted Dew Point Curve')
+plt.plot(x_actual, T_actual, label='Actual Bubble Point Curve')
+plt.plot(y_actual, T_actual, label='Actual Dew Point Curve')
+plt.title('T-XY for Ethanol-Water System at 32.86kPa')
+plt.xlabel('X or Y')
+plt.ylabel('T (K)')
+plt.xlim(0, 1)
+plt.legend()
+plt.grid()
+plt.show()
